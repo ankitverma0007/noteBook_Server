770d5a404401c298caeb8065739907284a451d40
ankit verma <ankivermajnva@gmail.com>
2026-02-19 00:38:02 +0530

    final commit

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
 const routes = require("./routes/routes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://notebook-seven-zeta.vercel.app/"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
+
+
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 
 app.use("/note", routes);
 app.use("/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Notebook API running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`open server at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
